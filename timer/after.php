c929f76425dbe839b4652bd0b5e502ccbf448aa6
yuanyueshui <yuanyueshui@wepiao.com>
2017-01-03 16:52:30 +0800

    timer

--- timer/after.php
+++ timer/after.php
@@ -1,7 +1,7 @@
 <?php
 /**
+ * 单次定时器
  * Created by PhpStorm.
- * User: yuanyueshui
  * Date: 2017/1/3
  * Time: 下午4:37
  */
@@ -16,15 +16,13 @@ function timeout($tm)
 }
 $timer1 = swoole_timer_after(1000, function () {
 	timeout(1);
-	//global $timer1, $timer3;
-	//swoole_timer_clear($timer1);
-	//swoole_timer_clear($timer3);
+	global $timer1, $timer3;
+	swoole_timer_clear($timer1);
+	swoole_timer_clear($timer3);
 });
 $timer2 = swoole_timer_after(2000, 'timeout', 2);
 $timer3 = swoole_timer_after(4000, 'timeout', 3);
-$timer4 = swoole_timer_after(5000, 'timeout', 4);
-$timer5 = swoole_timer_after(10000, 'timeout', 5);
-$timer6 = swoole_timer_after(5000, 'timeout', 6);
+
 //var_dump($timer1, $timer2, $timer3, $timer4, $timer5, $timer6);
 swoole_process::signal(SIGTERM, function() {
 	swoole_event_exit();
